18d40da00fcd424a3e65913d72f1692f1f16df14
likko <82750796+leukop@users.noreply.github.com>
2025-11-01 21:11:08 +0200

    Create install_basic_programs.bat

---
+++ install_basic_programs.bat
@@ -0,0 +1,8 @@
+:: Installs Office, 7zip, Chrome, Acrobat Reader, and Firefox
+
+winget install Microsoft.Office --accept-source-agreements --accept-package-agreements --disable-interactivity
+winget install Mozilla.Firefox --accept-source-agreements --accept-package-agreements --disable-interactivity
+winget install 7zip.7zip --accept-source-agreements --accept-package-agreements --disable-interactivity
+winget install Google.Chrome --accept-source-agreements --accept-package-agreements --disable-interactivity
+winget install Adobe.Acrobat.Reader.64-bit --accept-source-agreements --accept-package-agreements --disable-interactivity
+winget install vscodium --accept-source-agreements --accept-package-agreements --disable-interactivity
