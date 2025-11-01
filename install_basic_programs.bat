09242f7007205e527be52bbb1f377e4c8b38820e
likko <82750796+leukop@users.noreply.github.com>
2025-11-02 00:26:42 +0200

    Update install_basic_programs.bat

--- install_basic_programs.bat
+++ install_basic_programs.bat
@@ -3,6 +3,9 @@
 winget install Microsoft.Office --accept-source-agreements --accept-package-agreements --disable-interactivity
 winget install Mozilla.Firefox --accept-source-agreements --accept-package-agreements --disable-interactivity
 winget install 7zip.7zip --accept-source-agreements --accept-package-agreements --disable-interactivity
-winget install Google.Chrome --accept-source-agreements --accept-package-agreements --disable-interactivity
 winget install Adobe.Acrobat.Reader.64-bit --accept-source-agreements --accept-package-agreements --disable-interactivity
 winget install vscodium --accept-source-agreements --accept-package-agreements --disable-interactivity
+winget install Microsoft.Powershell --accept-source-agreements --accept-package-agreements --disable-interactivity
+winget install GnuPG.Gpg4win --accept-source-agreements --accept-package-agreements --disable-interactivity
+winget install Obsidian.Obsidian --accept-source-agreements --accept-package-agreements --disable-interactivity
+winget install VideoLAN.VLC --accept-source-agreements --accept-package-agreements --disable-interactivity
